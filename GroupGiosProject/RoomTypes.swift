aa2debb7585f117f521e57cc05aa47d67f5dd3dd
Alisha <alisha27011997@gmail.com>
2019-11-19 17:27:07 -0500

    Added RoomTypes Table view controller

---
+++ GroupGiosProject/RoomTypes.swift
@@ -0,0 +1,14 @@
+//
+//  RoomTypes.swift
+//  GroupGiosProject
+//
+//  Created by MacStudent on 2019-11-19.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+struct RoomTypes
+{
+    static var Types: [String] = ["Single ","Double ","Cabana ","Studio "]
+}
